d91d52bdcaa646dd55e8a2046d33b4b1719cc9d3
Ricardo Medina <71156336+Skydoes10@users.noreply.github.com>
2021-03-17 18:41:23 -0500

    Register.fxml created and Classes LaCasaDorada and LaCasaDoradaGUI

---
+++ La-Casa-Dorada-Restaurant/src/ui/LaCasaDoradaGUI.java
@@ -0,0 +1,30 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class LaCasaDoradaGUI {
+
+	//Log-In
+    @FXML
+    private BorderPane mainPanel;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private Button btnLogIn;
+
+    @FXML
+    private Button btnSignUp;
+    
+    
+    
+
+}
+
