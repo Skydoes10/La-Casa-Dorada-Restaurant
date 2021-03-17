ca69391ac351e443852c8f9f09712cc8ff080e11
Ricardo Medina <71156336+Skydoes10@users.noreply.github.com>
2021-03-17 18:40:24 -0500

    register.fxml created and Classes LaCasaDorada and LaCasaDoradaGUI

--- La-Casa-Dorada-Restaurant/src/model/User.java
+++ La-Casa-Dorada-Restaurant/src/model/User.java
@@ -2,11 +2,21 @@ package model;
 
 public class User extends Employee {
 
+	private String username;
 	private String password;
 	
-	public User(String n, String ln, int id, String pw) {
+	public User(String n, String ln, int id, String u, String pw) {
 		super(n, ln, id);
+		username = u;
 		password = pw;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getPassword() {
